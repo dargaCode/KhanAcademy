1fcb145faee48c8ad38e852fe6fb9e86d9edcce0
dargaCode <darga.code@gmail.com>
2016-11-30 16:06:11 -0800

    Add Khan Academy project links
    To constants and html template

--- js/constants.js
+++ js/constants.js
@@ -9,6 +9,7 @@ const CONSTANTS = (function() {
       'imageUrl': './img/project-thumbnails/bookshelf.png',
       'gitHubUrl': 'https://github.com/dargaCode/KhanBookShelf',
       'livePageUrl': 'http://dargacode.github.io/KhanBookShelf',
+      'khanUrl': 'https://www.khanacademy.org/computer-programming/book-shelf/6312048259629056',
     },
    {
       'name': 'Interactive Thunder Storm',
@@ -17,6 +18,7 @@ const CONSTANTS = (function() {
       'imageUrl': './img/project-thumbnails/thunder-storm.png',
       'gitHubUrl': 'https://github.com/dargaCode/KhanThunderStorm',
       'livePageUrl': 'http://dargacode.github.io/KhanThunderStorm',
+      'khanUrl': 'https://www.khanacademy.org/computer-programming/thunder-storm/6692749556449280',
     },
    {
       'name': 'Mansion Builder Animation',
@@ -25,6 +27,7 @@ const CONSTANTS = (function() {
       'imageUrl': './img/project-thumbnails/mansion-builder.png',
       'gitHubUrl': 'https://github.com/dargaCode/KhanMansionBuilder',
       'livePageUrl': 'http://dargacode.github.io/KhanMansionBuilder',
+      'khanUrl': 'https://www.khanacademy.org/computer-programming/mansion-builder/5011695120547840',
     },
    {
       'name': 'Magic 8-Ball',
@@ -33,6 +36,7 @@ const CONSTANTS = (function() {
       'imageUrl': './img/project-thumbnails/magic-8-ball.png',
       'gitHubUrl': 'https://github.com/dargaCode/KhanMagic8Ball',
       'livePageUrl': 'http://dargacode.github.io/KhanMagic8Ball',
+      'khanUrl': 'https://www.khanacademy.org/computer-programming/magic-8-ball/5857458956402688',
     },
    {
       'name': 'Randomized Fish Tank',
@@ -41,6 +45,7 @@ const CONSTANTS = (function() {
       'imageUrl': './img/project-thumbnails/fish-tank.png',
       'gitHubUrl': 'https://github.com/dargaCode/KhanFishTank',
       'livePageUrl': 'http://dargacode.github.io/KhanFishTank',
+      'khanUrl': 'https://www.khanacademy.org/computer-programming/the-noble-derpfish/5643580276342784',
     },
    {
       'name': 'Infinite Cat Attack',
@@ -49,14 +54,7 @@ const CONSTANTS = (function() {
       'imageUrl': './img/project-thumbnails/cat-attack.png',
       'gitHubUrl': 'https://github.com/dargaCode/KhanCatAttack',
       'livePageUrl': 'http://dargacode.github.io/KhanCatAttack',
-    },
-   {
-      'name': 'Mouse Interaction',
-      'description': 'Project Description Here',
-      'date': new Date('Jul 31 2015'),
-      'imageUrl': './img/project-thumbnails/mouse-interaction.png',
-      'gitHubUrl': 'https://github.com/dargaCode/KhanMouseInteraction',
-      'livePageUrl': 'http://dargacode.github.io/KhanMouseInteraction',
+      'khanUrl': 'https://www.khanacademy.org/computer-programming/infinite-cattack/4829025166032896',
     },
    {
       'name': 'Meteor Shower Animation',
@@ -65,7 +63,17 @@ const CONSTANTS = (function() {
       'imageUrl': './img/project-thumbnails/meteor-shower.png',
       'gitHubUrl': 'https://github.com/dargaCode/KhanMeteorShower',
       'livePageUrl': 'http://dargacode.github.io/KhanMeteorShower',
+      'khanUrl': 'https://www.khanacademy.org/computer-programming/shooting-star-meteor-shower/5786372654891008',
     },
+    {
+      'name': 'Mouse Interaction',
+      'description': 'Project Description Here',
+      'date': new Date('Jul 31 2015'),
+      'imageUrl': './img/project-thumbnails/mouse-interaction.png',
+      'gitHubUrl': 'https://github.com/dargaCode/KhanMouseInteraction',
+      'livePageUrl': 'http://dargacode.github.io/KhanMouseInteraction',
+      'khanUrl': 'https://www.khanacademy.org/computer-programming/mouse-interaction-pulsing-colors/5967973175132160',
+  },
    {
       'name': 'Chicken Drawing',
       'description': 'Project Description Here',
@@ -73,6 +81,7 @@ const CONSTANTS = (function() {
       'imageUrl': './img/project-thumbnails/chicken-drawing.png',
       'gitHubUrl': 'https://github.com/dargaCode/KhanChickenDrawing',
       'livePageUrl': 'http://dargacode.github.io/KhanChickenDrawing',
+      'khanUrl': 'https://www.khanacademy.org/computer-programming/wild-animal-chicken/5473107722764288',
     },
   ];
 
